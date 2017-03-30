f89675a3455be04a65a008eef4250f1e7b7ddc14
Artem <laiko2012@yandex.ua>
2017-03-30 20:23:04 +0300

    Upd

--- scripts/messages.js
+++ scripts/messages.js
@@ -3,14 +3,21 @@ import async from 'async';
 
 let doc = new GoogleSpreadsheet(process.env.workSheet)
 let worksheetinfo
+let conf = {
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url
+}
 
 let setAuth = function (callback) {
     async.series([
         function setAuth(step) {
-            doc.useServiceAccountAuth({
-                client_email: process.env.client_email,
-                private_key: process.env.private_key
-            }, step);
+            doc.useServiceAccountAuth(conf, step);
         },
         function getInfoAndWorksheets(step) {
             doc.getInfo(function(err, info) {
