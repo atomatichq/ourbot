56dda9667e31f9242eec7a85da632ceb58e3e763
Artem <laiko2012@yandex.ua>
2017-04-15 11:05:47 +0300

    Gists suppor added

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,9 +1,17 @@
 import GoogleSpreadsheet from 'google-spreadsheet';
 import * as creds from '../config.json';
 import async from 'async';
+import Gists from 'gists';
 
 let doc = new GoogleSpreadsheet(process.env.GOOGLE_WORKSHEET)
 let worksheetinfo
+
+let gists = new Gists({
+    username: process.env.GIST_USERNAME,
+    password: process.env.GIST_PASSWORD
+})
+
+
 
 let setAuth = function (callback) {
     async.series([
@@ -22,6 +30,14 @@ let setAuth = function (callback) {
     ], callback);
 }
 
+let sendGist = function (message, callback) {
+    gists.download({id: process.env.GIST_WORKSHEET }, function(err, res) {
+        gists.edit({"description": "the description for this gist", "files": {"log.txt": { "content": res.files["log.txt"].content + "\n" + formatGist(message) }}, "id": process.env.GIST_WORKSHEET }, function (err, inf) {
+            callback({"updated": res.updated_at})
+        })
+    });
+}
+
 let sendMessage = function (message, callback) {
     let messageObj = formatMessage(message)
     if (messageObj) {
@@ -32,6 +48,25 @@ let sendMessage = function (message, callback) {
         })
     }
     return "Specify parameters"
+}
+
+let formatGist = function (message) {
+    let msg = message.text
+    let action = message.text.match(/\+[^*\s]+/)[0]
+    msg = msg.replace(action, '').trim()
+    if(!msg) return null
+    let assignees = message.text.match(/@[^*\s]+/)
+    if (!assignees)
+      assignees = "none"
+    else {
+      assignees = assignees[0].trim()
+      msg = msg.replace(assignees, '').trim()
+    }
+
+    let name = message.user.name.substr(0, message.user.name.indexOf(' '))
+
+    return action.trim().substring(1) + " | " + new Date().toISOString() + " | " + "@" + message.user.login + " ("+ name +")" + " | " + assignees + " | " + msg
+
 }
 
 let formatMessage = function (message) {
@@ -60,5 +95,6 @@ let formatMessage = function (message) {
 
 exports.messages = {
     setAuth: setAuth,
-    sendMessage: sendMessage
+    sendMessage: sendMessage,
+    sendGist: sendGist
 };
