07d6cb748107bd017df6bbffdaf7a9001b1b9772
Artem <laiko2012@yandex.ua>
2017-04-25 16:35:19 +0300

    test improvements

--- test/parsing-test.js
+++ test/parsing-test.js
@@ -12,13 +12,20 @@ let helper = new Helper('../scripts/main.coffee')
 
 describe('Messages parsing', function () {
     let msg
+    let room
+    let a
+    let b
     beforeEach(function () {
-        this.room = helper.createRoom()
+        a = sinon.stub(messages, "sendMessage")
+        b = sinon.stub(messages, "sendGist")
+        room = helper.createRoom()
         msg = "+todo do @test this one"
     })
 
     afterEach(function () {
-        this.room.destroy()
+        room.destroy()
+        a.restore()
+        b.restore()
     })
 
     it('Action getting', function () {
@@ -47,10 +54,67 @@ describe('Messages parsing', function () {
     })
 
     it('Message Formatting', function () {
-        sinon.stub(formatting, 'getRoom').resolves({"name": "test"})
+        let t = sinon.stub(formatting, 'getRoom').resolves({"name": "test"})
         messages.formatMessage({"text": "+todo do this", "user": {"login":"test", "name": "Test (@Test)"}, "room": "sadqwewqeqw"}, function (res) {
             assert.equal(res.message, "do this")
         })
+        t.restore()
+    })
 
+    it('Message Formatting, with tag in middle', function () {
+        let t = sinon.stub(formatting, 'getRoom').resolves({"name": "test"})
+        messages.formatMessage({"text": "do +todo this", "user": {"login":"test", "name": "Test (@Test)"}, "room": "sadqwewqeqw"}, function (res) {
+            assert.equal(res.action, "todo")
+        })
+        t.restore()
+    })
+
+    it('Message Formatting, with tag in the end', function () {
+        let t = sinon.stub(formatting, 'getRoom').resolves({"name": "test"})
+        messages.formatMessage({"text": "do this +todo", "user": {"login":"test", "name": "Test (@Test)"}, "room": "sadqwewqeqw"}, function (res) {
+            assert.equal(res.action, "todo")
+        })
+        t.restore()
+    })
+
+    it('Gist formatting', function () {
+        messages.formatGist({"text": "+todo do this", "user": {"login":"test", "name": "Test (@Test)"}, "room": "sadqwewqeqw"}, function (res) {
+            assert.equal(res.substr(0, 6), "todo, ")
+        })
+    })
+
+    it('Typo in tag', function () {
+        room.user.say('weirdguy', "+tod do this one").then(function () {
+            assert.equal(a.callCount, 0)
+            assert.equal(b.callCount, 0)
+        })
+    })
+
+    it('Without typo in tag', function () {
+        room.user.say('weirdguy', "+todo do this one").then(function () {
+            assert.equal(a.callCount, 1)
+            assert.equal(b.callCount, 1)
+        })
+    })
+
+    it('Without any tag', function () {
+        room.user.say('weirdguy', "do this one").then(function () {
+            assert.equal(a.callCount, 0)
+            assert.equal(b.callCount, 0)
+        })
+    })
+
+    it('Tag in middle', function () {
+        room.user.say('weirdguy', "do +todo this one").then(function () {
+            assert.equal(a.callCount, 1)
+            assert.equal(b.callCount, 1)
+        })
+    })
+
+    it('Tag in the end', function () {
+        room.user.say('weirdguy', "do this one +todo").then(function () {
+            assert.equal(a.callCount, 1)
+            assert.equal(b.callCount, 1)
+        })
     })
 });
