bbe196f96599c55d5958dd18c850b785d1d546db
mikanebu <meiran1991@gmail.com>
2017-09-05 17:13:18 +0600

    [refactored-formatMessage][s]: refactored formatMessage function to use new structure for standup message  - refs #66

--- scripts/messages.js
+++ scripts/messages.js
@@ -33,6 +33,11 @@ let sendMessage = (message, dest, callback) => {
                     if (err) console.log(err)
                     callback(info)
                 }) 
+            } else if(!err && res.action === "standup"){
+                gdocs.addRow(info[1].worksheets[2].id, res, function (err, info) {
+                    if (err) console.log(err)
+                    callback(info)
+                })
             }
         })
     })
@@ -54,7 +59,9 @@ let formatMessage = (message, callback) => {
     let assignees = formatting.getDataMask(message.text, /@[^*\s]+/)
     let name = formatting.getName(message.user.name)
     let msg = formatting.removeFromMessage(message.text, action)
-
+    let blocker = formatting.getStandup(message.text,action, /blockers:(.*)last24/)
+    let last24 = formatting.getStandup(message.text, action, /last24:(.*)next24/)
+    let next24 = formatting.getStandup(message.text, action, /next24:(.*)/)
     formatting.getRoom(message.room).then(function (room) {
         callback({
             "action": action.substr(1),
@@ -62,7 +69,10 @@ let formatMessage = (message, callback) => {
             "poster": "@" + message.user.login + " ("+ name +")",
             "assignees": assignees,
             "message": formatting.removeFromMessage(msg, assignees),
-            "room": room.name
+            "room": room.name,
+            "blockers": blocker,
+            "last24": last24,
+            "next24": next24
         })
     })
 }
